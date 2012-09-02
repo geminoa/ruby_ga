8599dbb2a8577e4f75e4d667ae11c6a6824bb888
verdigrisbrain <geminoa@gmail.com>
2012-09-03 05:02:42 +0900

    Update rubyga_config

--- lib/rubyga_config.rb
+++ lib/rubyga_config.rb
@@ -4,8 +4,53 @@ class RubyGAConfig
 
   attr_accessor :gene_size, :gene_var, :gene, :desc
   def initialize(gene_size=nil, gene_var=nil, gene=nil, desc=nil)
-    if gene != nil || gene.class != Array
-      raise 'Class of gene must be Nil or Array.'
+    #if gene.class != Array
+    if !(gene == nil || gene.class == Array || gene.class == File || gene.class == String)
+      raise 'Class of gene must be NilClass or Array or File, String.'
+    end
+
+    if gene != nil
+      if gene.class == Array
+        @gene = gene
+      elsif gene.class == File
+        str = ""
+        str = gene.read
+        str.gsub!(/\n+/,"\n")
+        ary = []
+        str.each_line{|l|
+          ary << l.gsub(/\s/,"").chomp.split(',')
+        }
+        @gene = ary
+      elsif gene.class == String
+        str = ""
+        open(gene){|f| str += f.read}
+        str.gsub!(/\n+/,"\n")
+        ary = []
+        str.each_line{|l|
+          ary << l.gsub(/\s/,"").chomp.split(',')
+        }
+        @gene = ary
+      end
+    end
+
+    if gene_size != nil
+      @gene_size = gene_size
+    elsif @gene != nil
+      @gene_seze = @gene.size
+    else
+      @gene_size = $defaultGeneSize
+    end
+
+    if gene_var != nil
+      @gene_var = gene_var
+    elsif @gene != nil
+      if @gene[0].class == Array  # ２次元配列の場合
+        @gene_var = @gene[0].uniq
+      else  # 単純な配列の場合
+        @gene_var = @gene.uniq
+      end
+    else
+      @gene_var = $defaultGeneVar
     end
 
     if desc == nil
@@ -14,21 +59,6 @@ class RubyGAConfig
       @desc = desc
     end
 
-    if gene_size != nil
-      @gene_size = gene_size
-    elsif gene != nil
-      @gene_size = gene.size
-    else
-      @gene_size = $defaultGeneSize
-    end
-
-    if gene_var != nil
-      @gene_var = gene_var
-    elsif gene != nil
-      @gene_var = gene.uniq
-    else
-      @gene_var = $defaultGeneVar
-    end
   end
 
   # Generate gene to give the population.
@@ -37,14 +67,15 @@ class RubyGAConfig
   #   cond = condition of generating gene.
   # Condition is supposed as following,
   #   1. random (general purpose).
-  #   2. unique (it's intended for TSP(Travel Salesman Problem), so
-  #              each of element appears only once in the returned 
-  #              array which size equals given gene.)
+  #   2. tsp (it's intended for TSP(Travel Salesman Problem), so each of element appears
+  #           only once in the returned array which size equals given gene. 
+  #           The first element equals the last one.)
   def generate_gene(gene_num, cond=nil)
     if gene_num.class != Fixnum
       raise 'Class of gene_num must be Fixnum'
     end
 
+    genes = []
     gene_num.times do |num|
       bits = []
       cond = "random" if cond == nil
@@ -53,17 +84,14 @@ class RubyGAConfig
         @gene_size.times do |i|
           bits << @gene_var[rand(@gene_var.size)]
         end
-      elsif cond == "unique"
-        if gene.class != Array
-          raise 'You must give a gene as Array at initialize when the condition is "unique".'
-        end
-
-        tmp_gene = @gene.dup
-        @gene.size.times do |i|
+      elsif cond == "tsp"
+        tmp_gene = @gene_var.dup
+        @gene_var.size.times do |i|
           bits << tmp_gene.slice!(rand(tmp_gene.size))
         end
       end
+      genes << bits
     end
-    return bits
+    return genes
   end
 end
--- main.rb
+++ main.rb
@@ -1,6 +1,7 @@
 require "ruby_ga"
 require "pp"
 
+# test_simple_gaで使う評価関数
 def count_true(ary)
   sum = 0
   ary.each do |a|
@@ -9,15 +10,38 @@ def count_true(ary)
   return sum
 end
 
+# TPSで使う評価関数
+def distance(p1, p2)
+  return ((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
+end
+
+def generate_points(pnum=100, xrange=200, yrange=200)
+  points = []
+  pnum.times do |i|
+    points << [rand(xrange) - xrange/2, rand(yrange) - yrange/2]
+  end
+  points << points[0].dup
+  return points
+end
+
 def main
+  points = generate_points(20)
+  (points.size-1).size.times do |i|
+    p distance(points[i], points[i+1])
+  end
+  po = Population.new
+  pp po.units.size
+end
+
+def test_simple_ga
   ["roulette", "elite", "tournament", "rank"].each do |sel|
     gpl_cmd = "plot"
     ["inversion", "translocation", "move", "scramble", "else"].each do |mut|
-      po = Population.new 50
+      po = Population.new
       fun = method(:count_true)
       p po.average_fitness(fun)
       file = open("dat/evo_#{sel}_#{mut}.dat", "w+")
-      1000.times do |i|
+      2000.times do |i|
         po.simple_ga(fun, sel, mut)
         #po.modified_ga(fun)
         #puts "fit: " + po.elite_selection(fun).fitness(fun).to_s
@@ -26,6 +50,7 @@ def main
       file.close
       gpl_cmd += " 'evo_#{sel}_#{mut}.dat' w l,"
     end
+    gpl_cmd.chop!
     open("dat/#{sel}.gpl", "w+") {|f| f.write(gpl_cmd)}
   end
 end
