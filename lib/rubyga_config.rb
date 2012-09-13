9c6b47f2beeb11a0ec9b4ca81bb380614d28db72
verdigrisbrain <geminoa@gmail.com>
2012-09-14 08:32:11 +0900

    Refactor RubyGAConfig to check the gene type

--- lib/rubyga_config.rb
+++ lib/rubyga_config.rb
@@ -6,72 +6,80 @@ class RubyGAConfig
   $defaultMutation = nil
   $defaultCrossover = "uniform"
 
-  attr_accessor :unit_num, :gene_size, :gene_var, :gene, :fitness, :selection, :mutation, :crossover, :desc
+  attr_accessor :unit_num, :gene_size, :gene_var, :genes, :fitness, :selection, :mutation, :crossover, :desc
   def initialize(
     unit_num=nil,   # Number of unit (or individual) of the population.
     gene_size=nil,  # Size of gene of an individual.
     gene_var=nil,   # Gene variation, for exp. [true,false] or [0,1,2,...].
-    gene=nil,       # You can give all of the gene directory as multi-dimension array.
+    genes=nil,       # You can give all of the gene directory as multi-dimension array.
     fitness=nil,    # Fitness funciton.
     selection=nil,  # Selection method (roulette, rank, elite, tournament).
     mutation=nil,   # Mutation method (inversion, translocation, move, scramble or nil(examine each bit of gene to mutate) ).
     crossover=nil,  # Crossover method. Default is uniform crossover. You can give the argument as Fixnum(single or multi-point crossover).
     desc=nil        # Description.
   )
-  #if gene.class != Array
-  if !(gene == nil || gene.class == Array || gene.class == File || gene.class == String)
+  #if genes.class != Array
+  if !(genes == nil || genes.class == Array || genes.class == File || genes.class == String)
     raise 'Class of gene must be NilClass or Array or File, String.'
   end
 
-  if gene != nil
-    if gene.class == Array
-      @gene = gene
-    elsif gene.class == File
+  if unit_num == nil
+    @unit_num = $defaultUnitNum
+  elsif unit_num.class == Fixnum
+    @unit_num = unit_num
+  else
+    raise 'Class of unit_num must be Fixnum'
+  end
+
+  if genes != nil
+    if genes.class == Array
+      @genes = genes
+    elsif genes.class == File
       str = ""
-      str = gene.read
+      str = genes.read
       str.gsub!(/\n+/,"\n")
       ary = []
       str.each_line{|l|
         ary << l.gsub(/\s/,"").chomp.split(',')
       }
-      @gene = ary
-    elsif gene.class == String
+      @genes = ary
+    elsif genes.class == String
       str = ""
-      open(gene){|f| str += f.read}
+      open(genes){|f| str += f.read}
       str.gsub!(/\n+/,"\n")
       ary = []
       str.each_line{|l|
         ary << l.gsub(/\s/,"").chomp.split(',')
       }
-      @gene = ary
+      @genes = ary
     end
 
-    # Check the class of @gene.
-    if @gene.class != Array
-      raise 'Class of gene must be Array.'
+    # Check the class of @genes.
+    if @genes.class != Array
+      raise 'Class of genes must be Array.'
     end
 
     if gene_var != nil
       @gene_var = gene_var
     else
-      if @gene[0].class == Array  # ２次元配列の場合
-        @gene_var = @gene[0].uniq
+      if @genes[0].class == Array  # ２次元配列の場合
+        @gene_var = @genes[0].uniq
       else   # 単純な配列の場合
-        @gene_var = @gene.uniq
+        @gene_var = @genes.uniq
       end
     end
 
     if gene_size != nil
       @gene_size = gene_size
     else
-      if @gene[0].class == Array  # ２次元配列の場合
-        @gene_var = @gene[0].uniq
+      if @genes[0].class == Array  # ２次元配列の場合
+        @gene_var = @genes[0].uniq
       else   # 単純な配列の場合
-        @gene_var = @gene.uniq
+        @gene_var = @genes.uniq
       end
     end
 
-  else  # case of gene == nil.
+  else  # case of genes == nil.
     if gene_size != nil
       @gene_size = gene_size
     else
@@ -83,14 +91,11 @@ class RubyGAConfig
     else
       @gene_var = $defaultGeneVar
     end
-  end
 
-  if unit_num == nil
-    @unit_num = $defaultUnitNum
-  elsif unit_num.class == Fixnum
-    @unit_num = unit_num
-  else
-    raise 'Class of unit_num must be Fixnum'
+    @genes = []
+    @unit_num.times do |i|
+      @genes << generate_gene()
+    end
   end
 
   if mutation == nil
@@ -141,5 +146,6 @@ class RubyGAConfig
         bits << tmp_gene.slice!(rand(tmp_gene.size))
       end
     end
+    return bits
   end
 end
